9f3634f040c83580437de9fa30160c1abd2acd70
alphazhangyz <alphazhangyz@gmail.com>
2021-04-05 14:39:47 +0800

    增加自定义options

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 
 const program = require('commander')
 
+const helpOptions = require('./lib/core/help')
+
 const _VERSION = require('./package.json').version
 
 program.version(_VERSION)
+
+// 增加自定义 options
+helpOptions()
+
+
 
 program.parse(process.argv)
