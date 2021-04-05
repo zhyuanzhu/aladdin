dad97ff4a37d9fae2ea380f07759812201d566bf
alphazhangyz <alphazhangyz@gmail.com>
2021-04-05 14:24:34 +0800

    Commander.js 添加，设置版本号

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('test')
+const program = require('commander')
+
+const _VERSION = require('./package.json').version
+
+program.version(_VERSION)
+
+program.parse(process.argv)
