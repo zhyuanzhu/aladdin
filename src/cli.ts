4c986be426b28ed85179c7168208ef84976c4f95
alphazhangyz <alphazhangyz@gmail.com>
2021-08-17 15:56:48 +0800

    基础命令和说明文档

--- src/cli.ts
+++ src/cli.ts
@@ -2,3 +2,13 @@ import cac from "cac";
 import { name, version } from '../package.json';
 
 const cli = cac(name);
+
+cli.command('<template> [project]', '创建一个新模版项目')
+  .option('-f, --force', '覆盖原有项目')
+  .option('-o, --offline', '尝试加载本地模版')
+  .allowUnknownOptions()
+  .example('    #xx  使用 `#` 开头加载一个远程模版库中的模版 ')
+  .example('    ./xx 使用 `./` 相对路径加载一个本地模版')
+  .example('    使用 `https?://` 开头的绝对路径加载一个远程模版')
+
+cli.help().version(version).parse()
